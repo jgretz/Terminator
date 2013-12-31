0e73d647df4d84c8cf327720df9b7fc1d2f6ec1c
jgretz <jgretz@truefitsolutions.com>
2013-12-31 15:11:03 -0500

    Kills Zombies and adds square cam class

---
+++ Terminator/Services/SquareCam.h
@@ -0,0 +1,13 @@
+//
+//  SquareCam.h
+//  Terminator
+//
+//  Created by Joshua Gretz on 12/31/13.
+//  Copyright (c) 2013 gretz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SquareCam : NSObject
+
+@end
---
+++ Terminator/Services/SquareCam.m
@@ -0,0 +1,13 @@
+//
+//  SquareCam.m
+//  Terminator
+//
+//  Created by Joshua Gretz on 12/31/13.
+//  Copyright (c) 2013 gretz. All rights reserved.
+//
+
+#import "SquareCam.h"
+
+@implementation SquareCam
+
+@end
